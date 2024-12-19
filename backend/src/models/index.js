8c81939c09b892add1e014e2989a13c0bc20e1f5
KhoaHo <86819208+it-raining@users.noreply.github.com>
2024-12-19 21:16:44 +0700

    backend 0.1

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,12 +1,17 @@
 import { MongoClient } from "mongodb";
+import 'dotenv/config'
 
 let db;
 
 export const connectDB = async () => {
   try {
+    console.log("start");
+    console.log(process.env.MONGO_URI);
     const client = new MongoClient(process.env.MONGO_URI);
+    console.log(client);
+    console.log("client connected");
     await client.connect();
-    db = client.db("SSPS");
+    db = client.db(process.env.DB_NAME);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error(`Error: ${error.message}`);
