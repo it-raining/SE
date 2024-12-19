cbe2491a8383eb3bfe002a1244c466becd3af9ed
tuanacc2 <167599943+tuanacc2@users.noreply.github.com>
2024-12-19 14:17:43 +0700

    Delete frontend/src/pages directory

--- frontend/src/pages/PrintList.js
+++
@@ -1,8 +0,0 @@
-export const PrintList = [
-    {
-        title: "Tôi thấy hoa vàng trên cỏ xanh.docx, Đồi gió hú.pdf",
-    },
-    {
-        title: "omaigottoitsuaniiga.pdf, giáo trình.pdf, slide1.pdf, slide2.pdf, slide3.pdf ",
-    }
-]
