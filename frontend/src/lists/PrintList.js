d127c16b9b7e7da4f17444ec0f4bd0a78e87b0d7
tuanacc2 <167599943+tuanacc2@users.noreply.github.com>
2024-12-24 20:52:36 +0700

    Delete frontend/src/lists directory

--- frontend/src/lists/PrintList.js
+++
@@ -1,8 +0,0 @@
-export const PrintList = [
-    {
-        title: "Tôi thấy hoa vàng trên cỏ xanh.docx, Đồi gió hú.pdf",
-    },
-    {
-        title: "giáo trình.pdf, slide1.pdf, slide2.pdf, slide3.pdf ",
-    }
-]
